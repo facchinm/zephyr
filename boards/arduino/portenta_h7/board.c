f2d323e5704ff87c3b30e448d9e34c748dbb4418
Martino Facchin <m.facchin@arduino.cc>
2024-07-11 14:18:36 +0200

    portenta_h7: enable ethernet driver

--- boards/arduino/portenta_h7/board.c
+++ boards/arduino/portenta_h7/board.c
@@ -6,6 +6,7 @@
 
 #include <zephyr/init.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/i2c.h>
 
 static int board_init(void)
 {
@@ -17,7 +18,25 @@ static int board_init(void)
 		return -ENODEV;
 	}
 
-	return gpio_pin_configure_dt(&led1, GPIO_OUTPUT_INACTIVE);
+	gpio_pin_configure_dt(&led1, GPIO_OUTPUT_INACTIVE);
+
+#define PMIC_I2C DT_NODELABEL(i2c1)
+
+#if DT_NODE_HAS_STATUS(PMIC_I2C, okay)
+	const struct device *const i2c_dev = DEVICE_DT_GET(PMIC_I2C);
+	if (!device_is_ready(i2c_dev)) {
+		return -ENODEV;
+	}
+	uint32_t i2c_cfg = I2C_SPEED_SET(I2C_SPEED_STANDARD) | I2C_MODE_CONTROLLER;
+	i2c_configure(i2c_dev, i2c_cfg);
+
+	i2c_reg_write_byte(i2c_dev, 8 << 1, 0x52, 0x9);
+	i2c_reg_write_byte(i2c_dev, 8 << 1, 0x53, 0xF);
+	i2c_reg_write_byte(i2c_dev, 8 << 1, 0x3B, 0xF);
+	i2c_reg_write_byte(i2c_dev, 8 << 1, 0x35, 0xF);
+#endif
+
+	return 0;
 }
 
 SYS_INIT(board_init, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
