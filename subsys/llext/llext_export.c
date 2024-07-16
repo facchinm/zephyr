0f4cfc85b3c1924ab152b538e65603923884fea4
Martino Facchin <m.facchin@arduino.cc>
2024-07-16 16:41:50 +0200

    llext: export symbols from libc and libgcc

--- subsys/llext/llext_export.c
+++ subsys/llext/llext_export.c
@@ -6,6 +6,9 @@
 
 #include <string.h>
 #include <zephyr/llext/symbol.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define FORCE_EXPORT_SYM(name) \
 	extern void name(void); \
@@ -13,22 +16,65 @@
 
 
 EXPORT_SYMBOL(strcpy);
-EXPORT_SYMBOL(strncpy);
-EXPORT_SYMBOL(strlen);
-EXPORT_SYMBOL(strcmp);
+EXPORT_SYMBOL(strrchr);
+EXPORT_SYMBOL(strstr);
 EXPORT_SYMBOL(strncmp);
+EXPORT_SYMBOL(strncpy);
+EXPORT_SYMBOL(strcmp);
+EXPORT_SYMBOL(strlen);
+EXPORT_SYMBOL(strchr);
+
 EXPORT_SYMBOL(memcmp);
 EXPORT_SYMBOL(memcpy);
 EXPORT_SYMBOL(memset);
+EXPORT_SYMBOL(memmove);
+
+EXPORT_SYMBOL(malloc);
+EXPORT_SYMBOL(realloc);
+EXPORT_SYMBOL(calloc);
+EXPORT_SYMBOL(free);
+EXPORT_SYMBOL(rand);
+EXPORT_SYMBOL(srand);
+
+EXPORT_SYMBOL(atof);
+EXPORT_SYMBOL(atol);
+EXPORT_SYMBOL(isspace);
+EXPORT_SYMBOL(isalnum);
+EXPORT_SYMBOL(tolower);
+EXPORT_SYMBOL(toupper);
+
+EXPORT_SYMBOL(sprintf);
+EXPORT_SYMBOL(snprintf);
+FORCE_EXPORT_SYM(cbvprintf);
 
 FORCE_EXPORT_SYM(abort);
-FORCE_EXPORT_SYM(cbvprintf);
-EXPORT_SYMBOL(free);
-FORCE_EXPORT_SYM(rand);
 FORCE_EXPORT_SYM(ring_buf_get);
 FORCE_EXPORT_SYM(ring_buf_peek);
 FORCE_EXPORT_SYM(ring_buf_put);
-FORCE_EXPORT_SYM(srand);
 FORCE_EXPORT_SYM(sys_clock_cycle_get_32);
+FORCE_EXPORT_SYM(__aeabi_dcmpun);
+FORCE_EXPORT_SYM(__aeabi_dcmple);
+FORCE_EXPORT_SYM(__aeabi_d2lz);
+FORCE_EXPORT_SYM(__aeabi_uldivmod);
+FORCE_EXPORT_SYM(__aeabi_ui2d);
+FORCE_EXPORT_SYM(__aeabi_dcmplt);
+FORCE_EXPORT_SYM(__aeabi_ddiv);
+FORCE_EXPORT_SYM(__aeabi_dmul);
+FORCE_EXPORT_SYM(__aeabi_d2f);
+FORCE_EXPORT_SYM(__aeabi_fcmpun);
+FORCE_EXPORT_SYM(__aeabi_dadd);
+FORCE_EXPORT_SYM(__aeabi_fcmple);
+FORCE_EXPORT_SYM(__aeabi_idiv);
+FORCE_EXPORT_SYM(__aeabi_dcmpgt);
+FORCE_EXPORT_SYM(__aeabi_dsub);
+FORCE_EXPORT_SYM(__aeabi_i2d);
+FORCE_EXPORT_SYM(__aeabi_uidiv);
+FORCE_EXPORT_SYM(__aeabi_l2d);
+FORCE_EXPORT_SYM(__aeabi_d2uiz);
+FORCE_EXPORT_SYM(__aeabi_uidivmod);
+FORCE_EXPORT_SYM(__aeabi_dcmpeq);
+FORCE_EXPORT_SYM(__aeabi_d2iz);
+FORCE_EXPORT_SYM(__aeabi_f2d);
+FORCE_EXPORT_SYM(__aeabi_idivmod);
 
 #include <zephyr/syscall_export_llext.c>
