dfbb4a0ec6092f446f9a57d19e63f325e5bf3124
Martino Facchin <m.facchin@arduino.cc>
2025-01-17 09:39:01 +0100

    uart: nxp: fix hang on bluetooth init

--- drivers/serial/uart_mcux_lpuart.c
+++ drivers/serial/uart_mcux_lpuart.c
@@ -1167,7 +1167,7 @@ static int mcux_lpuart_configure(const struct device *dev,
 	config->base->CTRL &= ~LPUART_CTRL_RSRC_MASK;
 
 	/* disable LPUART */
-	LPUART_Deinit(config->base);
+	// LPUART_Deinit(config->base);
 
 	int ret = mcux_lpuart_configure_init(dev, cfg);
 	if (ret) {
