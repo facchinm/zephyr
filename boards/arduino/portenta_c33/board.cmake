e6223698222d36a625aacd35e655d3b97432fee1
Martino Facchin <m.facchin@arduino.cc>
2025-02-19 17:21:39 +0100

    portenta_c33: add jlink and remove default cdc

--- boards/arduino/portenta_c33/board.cmake
+++ boards/arduino/portenta_c33/board.cmake
@@ -4,5 +4,7 @@
 
 # FIXME: Arduino dfu-util provides -Q to reset the board after flashing
 board_runner_args(dfu-util "--pid=2341:0368" "--alt=0")
+board_runner_args(jlink "--device=R7FA6M5BH")
 
 include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
