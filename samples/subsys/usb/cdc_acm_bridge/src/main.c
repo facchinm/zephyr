4af38cb6fe2fbfb69c7b4d58b01f249e9bfbb06f
Martino Facchin <m.facchin@arduino.cc>
2026-06-23 17:21:32 +0200

    temp: mezza: cdc_acm_bridge: allow flashing Radio via QConn_Eflash-ubuntu

--- samples/subsys/usb/cdc_acm_bridge/src/main.c
+++ samples/subsys/usb/cdc_acm_bridge/src/main.c
@@ -7,8 +7,8 @@
 #include <sample_usbd.h>
 
 #include <stdio.h>
-#include <string.h>
 #include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/uart/uart_bridge.h>
 #include <zephyr/kernel.h>
 
@@ -20,6 +20,21 @@ LOG_MODULE_REGISTER(cdc_acm_bridge, LOG_LEVEL_INF);
 const struct device *const uart_dev = DEVICE_DT_GET_ONE(zephyr_cdc_acm_uart);
 
 static struct usbd_context *sample_usbd;
+
+#define WIFI_BOOT_NODE DT_NODELABEL(wifi_boot_gpio)
+#define WIFI_EN_NODE   DT_NODELABEL(wifi_chip_en_gpio)
+
+#if DT_NODE_EXISTS(WIFI_BOOT_NODE) && DT_NODE_EXISTS(WIFI_EN_NODE)
+#define WIFI_GPIO_SPEC_FROM_LOCAL_CELLS(node_id)                                                   \
+	{                                                                                            \
+		.port = DEVICE_DT_GET(DT_PARENT(node_id)),                                            \
+		.pin = DT_PROP_BY_IDX(node_id, gpios, 0),                                             \
+		.dt_flags = DT_PROP_BY_IDX(node_id, gpios, 1),                                        \
+	}
+
+static const struct gpio_dt_spec wifi_boot = WIFI_GPIO_SPEC_FROM_LOCAL_CELLS(WIFI_BOOT_NODE);
+static const struct gpio_dt_spec wifi_en = WIFI_GPIO_SPEC_FROM_LOCAL_CELLS(WIFI_EN_NODE);
+#endif
 
 #define DEVICE_DT_GET_COMMA(node_id) DEVICE_DT_GET(node_id),
 
@@ -59,6 +74,19 @@ static void sample_msg_cb(struct usbd_context *const ctx, const struct usbd_msg 
 int main(void)
 {
 	int err;
+
+#if DT_NODE_EXISTS(WIFI_BOOT_NODE) && DT_NODE_EXISTS(WIFI_EN_NODE)
+	if (!gpio_is_ready_dt(&wifi_boot) || !gpio_is_ready_dt(&wifi_en)) {
+		LOG_ERR("Wi-Fi GPIO controller not ready");
+		return -ENODEV;
+	}
+
+	gpio_pin_configure_dt(&wifi_en, GPIO_OUTPUT);
+	gpio_pin_configure_dt(&wifi_boot, GPIO_OUTPUT);
+	gpio_pin_set_dt(&wifi_boot, 1);
+	gpio_pin_set_dt(&wifi_en, 0);
+	gpio_pin_set_dt(&wifi_en, 1);
+#endif
 
 	sample_usbd = sample_usbd_init_device(sample_msg_cb);
 	if (sample_usbd == NULL) {
